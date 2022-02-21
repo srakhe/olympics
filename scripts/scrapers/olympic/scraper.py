b5175e0eb3b793917eae9b79439a8d019752b409
srakhe <s.rakhe@protonmail.ch>
2022-02-21 14:18:05 -0800

    Added country codes scraper

--- scripts/scrapers/olympic/scraper.py
+++ scripts/scrapers/olympic/scraper.py
@@ -1,4 +1,5 @@
 from scripts.scrapers.utils import ScrapingUtils
+from country_codes_scraper import CountryCodesScraper
 
 
 class OlympicScraper:
@@ -7,10 +8,12 @@ class OlympicScraper:
         scr_utils = ScrapingUtils(url=url)
         self.can_crawl = scr_utils.check_crawl_allowed()
         self.crawl_delay = scr_utils.get_crawl_delay()
+        self.cc_scraper = CountryCodesScraper(data_path="data", url="https://www.olympedia.org/countries")
 
     def scrape(self):
         if self.can_crawl:
             print("Crawl is possible.")
+            self.cc_scraper.run_scrape()
         else:
             print("It is not possible to crawl this page.")
 
