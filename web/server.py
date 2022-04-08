9dd137fd938d5ea86e3ca5e68f7b6ee4ab6bd027
srakhe <s.rakhe@protonmail.ch>
2022-04-07 18:31:47 -0700

    Sentiment analysis UI done

--- web/server.py
+++ web/server.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from utils.sentiment_analysis import SentimentAnalysis
 
 app = Flask(__name__)
+snt_util = SentimentAnalysis(data_path="../data")
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/sentiment_analysis", methods=["GET", "POST"])
+def sentiment_analysis():
+    if request.method == "GET":
+        params = {}
+        games_options = snt_util.get_olympic_options()
+        params["games"] = games_options
+        return render_template("sentiments.html", params=params)
+    else:
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
---
+++ web/utils/sentiment_analysis.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+class SentimentAnalysis:
+
+    def __init__(self, data_path):
+        self.data_path = data_path
+
+    def get_olympic_options(self):
+        games_df = pd.read_csv(f"{self.data_path}/games.csv")
+        games_df = games_df[games_df["year"] >= 2004]
+        games_df = games_df[games_df["year"] <= 2022]
+        games_df["options"] = "[" + games_df["Type"] + "] " + games_df["year"].astype(str) + " in " + games_df[
+            "city"].astype(str) + ", " + games_df["country_code"].astype(str)
+        games_options = games_df["options"].to_list()
+        return games_options
